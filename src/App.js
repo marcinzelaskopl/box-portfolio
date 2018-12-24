60e9210e8a81e9191f4dc2ca7783778d868015c1
marcinzelaskopl <marcinzelaskov@gmail.com>
2018-12-24 21:00:35 +0100

    nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.scss';
+import Nav from './components/Nav';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Nav/>
       </div>
     );
   }
---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import '../App.scss';
+
+class Nav extends Component {
+    render() {
+        return (
+            <div className='grid-container'>
+                <div className='row'>
+                    <div className='col-12'>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Nav
