a7935771e80f2a40a6bbf6075eecbed3db7b77e8
marcinzelaskopl <marcinzelaskov@gmail.com>
2018-12-28 16:54:06 +0100

    projects comp

---
+++ src/components/Projects.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+class Projects extends Component {
+    render() {
+        return (
+            <div className='grid-container'>
+                <div className='row'>
+                    <div className='col-12 projects'>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Projects
